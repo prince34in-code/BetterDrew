fd112172252ed186dd8ce8d5b5d80500c3560ed6
Prince Kumar <prince.kumar@gmail.com>
2026-08-16 02:00:53 +0530

    Fix GitHub Pages base path

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import './styles/index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
     </HelmetProvider>
